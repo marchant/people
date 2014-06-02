b82729c4ce574edf581ce558b132bbcc84ccefcf
Roman Cortes <javier.roman.c@gmail.com>
2014-06-02 18:36:46 +0200

    Update main page

--- ui/facebook-logo.reel/facebook-logo.js
+++ ui/facebook-logo.reel/facebook-logo.js
@@ -13,5 +13,36 @@ exports.FacebookLogo = Component.specialize(/** @lends FacebookLogo# */ {
         value: function FacebookLogo() {
             this.super();
         }
+    },
+
+    _opacity: {
+        value: 1
+    },
+
+    opacity: {
+        get: function () {
+            return this._opacity;
+        },
+        set: function (value) {
+            var opacity = parseFloat(value);
+
+            if (opacity < 0) {
+                opacity = 0;
+            }
+            if (opacity > 1) {
+                opacity = 1;
+            }
+            opacity = (Math.cos((1 - opacity) * Math.PI) + 1) * .5;
+            if (this._opacity !== opacity) {
+                this._opacity = opacity;
+                this.needsDraw = true;
+            }
+        }
+    },
+
+    draw: {
+        value: function () {
+            this.logo.style.opacity = this._opacity;
+        }
     }
 });
