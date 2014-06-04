8e0c662588f5d25137cf1d3653840e2a39e46fad
François Frisch <francoisfrisch@gmail.com>
2014-06-04 11:54:17 -0700

    Remove data padding

--- core/user-controller.js
+++ core/user-controller.js
@@ -86,39 +86,39 @@ exports.UserController = FacebookController.specialize({
                     feed.forEach(function (post) {
                         postControllers.push(new PostController(post, service._facebook));
                     });
-                    feed.forEach(function (post) {
-                        postControllers.push(new PostController(post, service._facebook));
-                    });
-                    feed.forEach(function (post) {
-                        postControllers.push(new PostController(post, service._facebook));
-                    });
-                    feed.forEach(function (post) {
-                        postControllers.push(new PostController(post, service._facebook));
-                    });
-                    feed.forEach(function (post) {
-                        postControllers.push(new PostController(post, service._facebook));
-                    });
-                    feed.forEach(function (post) {
-                        postControllers.push(new PostController(post, service._facebook));
-                    });
-                    feed.forEach(function (post) {
-                        postControllers.push(new PostController(post, service._facebook));
-                    });
-                    feed.forEach(function (post) {
-                        postControllers.push(new PostController(post, service._facebook));
-                    });
-                    feed.forEach(function (post) {
-                        postControllers.push(new PostController(post, service._facebook));
-                    });
-                    feed.forEach(function (post) {
-                        postControllers.push(new PostController(post, service._facebook));
-                    });
-                    feed.forEach(function (post) {
-                        postControllers.push(new PostController(post, service._facebook));
-                    });
-                    feed.forEach(function (post) {
-                        postControllers.push(new PostController(post, service._facebook));
-                    });
+//                    feed.forEach(function (post) {
+//                        postControllers.push(new PostController(post, service._facebook));
+//                    });
+//                    feed.forEach(function (post) {
+//                        postControllers.push(new PostController(post, service._facebook));
+//                    });
+//                    feed.forEach(function (post) {
+//                        postControllers.push(new PostController(post, service._facebook));
+//                    });
+//                    feed.forEach(function (post) {
+//                        postControllers.push(new PostController(post, service._facebook));
+//                    });
+//                    feed.forEach(function (post) {
+//                        postControllers.push(new PostController(post, service._facebook));
+//                    });
+//                    feed.forEach(function (post) {
+//                        postControllers.push(new PostController(post, service._facebook));
+//                    });
+//                    feed.forEach(function (post) {
+//                        postControllers.push(new PostController(post, service._facebook));
+//                    });
+//                    feed.forEach(function (post) {
+//                        postControllers.push(new PostController(post, service._facebook));
+//                    });
+//                    feed.forEach(function (post) {
+//                        postControllers.push(new PostController(post, service._facebook));
+//                    });
+//                    feed.forEach(function (post) {
+//                        postControllers.push(new PostController(post, service._facebook));
+//                    });
+//                    feed.forEach(function (post) {
+//                        postControllers.push(new PostController(post, service._facebook));
+//                    });
                     return postControllers;
                 })
                 .then(function (postControllers) {
