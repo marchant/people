12f6ce94b9a8f3e60e46c3fe4b91c952d9e6f759
François Frisch <francoisfrisch@gmail.com>
2014-06-19 22:36:33 -0700

    Make Zoom work with <img> element

--- ui/detail.reel/detail.js
+++ ui/detail.reel/detail.js
@@ -21,25 +21,12 @@ exports.Detail = Component.specialize(/** @lends Detail# */ {
     show: {
         value: function (startElement) {
             if(window.webkitConvertPointFromNodeToPage) {
-                var startWidth = startElement.offsetWidth;
-                var startHeight = startElement.offsetHeight;
-                var endWidth = document.documentElement.clientWidth;
-                var endHeight = document.documentElement.clientHeight;
-                var startTopLeft = window.webkitConvertPointFromNodeToPage(startElement, new window.WebKitPoint(0, 0));
-                var startBottomRight = window.webkitConvertPointFromNodeToPage(startElement, new window.WebKitPoint(startWidth, startHeight));
-                var endBottomRight = new window.WebKitPoint(endWidth, endHeight);
-
-                this.startX = startTopLeft.x;
-                this.startY = startTopLeft.y;
-                this.scaleX = (startBottomRight.x-startTopLeft.x)/endBottomRight.x;
-                this.scaleY = (startBottomRight.y-startTopLeft.y)/endBottomRight.y;
+                this.point = window.webkitConvertPointFromNodeToPage(startElement, new window.WebKitPoint(0, 0));
             } else {
-                this.startX = 0;
-                this.startY = 0;
-                this.scaleX = 0.1;
-                this.scaleY = 0.1;
+                this.point = {x:0,y:0};
             }
             this._startElement = startElement;
+            this._showOverlay = true;
             this.needsDraw = true;
 
         }
@@ -58,37 +45,147 @@ exports.Detail = Component.specialize(/** @lends Detail# */ {
 
     willDraw: {
         value: function () {
-            if(this._startElement) {
-                this._clonedElement = this._copyNodes(this._startElement);
-                // only do it once
-                this._startElement = null;
+            if(this._startAnimation) {
+                this.elementWidth = this._startElement.offsetWidth;
+                this.elementHeight = this._startElement.offsetHeight;
+                this.imageWidth = this.postController.imageSmall.width;
+                this.imageHeight = this.postController.imageSmall.height;
+                this.screenWidth = this.zoomedImageWrapper.offsetWidth;
+                this.screenHeight = this.zoomedImageWrapper.offsetHeight;
             }
-            this.detailPhotoRatio = this.detailPhoto.offsetWidth / this.detailPhoto.offsetHeight;
         }
     },
 
     draw: {
         value: function () {
-            var self = this;
-            var detailOverlay = this.detailOverlay;
-            if (this.scaleX !== null) {
-                detailOverlay.style.webkitTransform = ["translate3d(",this.startX,"px,",this.startY,"px, 0px) scale(",this.scaleX,",",this.scaleY ,")"].join('');
-                this.templateObjects.overlay.show();
-                this.scaleX = null;
-                self._animationFinished = false;
-                detailOverlay.classList.add("is-booting");
-            } else if(this._startAnimation) {
-                var naturalRatio = this.postController.imageSmall.width / this.postController.imageSmall.height;
-                if (naturalRatio < this.detailPhotoRatio) {
-                    detailOverlay.classList.add("is-tall");
-                } else {
-                    detailOverlay.classList.remove("is-tall");
-                }
-                detailOverlay.style.webkitTransform = "";
-                detailOverlay.classList.remove("is-booting");
-                this._startAnimation = false;
-//                setTimeout(this.handleCloseAction.bind(this), 700);
+            var zoomedImageWrapper = this.zoomedImageWrapper,
+            clippedZoomedImage = this.clippedZoomedImage,
+            zoomedImage = this.templateObjects.imageSmall.element,
+            spacer = this.spacer,
+            clip1 = this.clip1,
+            clip2 = this.clip2,
+            clip,
+            imageWidth = this.imageWidth,
+            imageHeight = this.imageHeight,
+            elementWidth = this.elementWidth,
+            elementHeight = this.elementHeight,
+            point,
+            elementX,
+            elementY,
+            uncroppedX,
+            uncroppedY,
+            screenWidth,
+            screenHeight,
+            uncroppedWidth,
+            uncroppedHeight,
+            targetWidth,
+            targetHeight,
+            zoom;
 
+            if(this._showOverlay) {
+                console.log("_showOverlay");
+                this._showOverlay = false;
+
+                this.templateObjects.overlay.show();
+
+
+                //setup all the flags
+                this._animationFinished = false;
+                this._startAnimation = false;
+                this._animationStepTwo = false;
+                this._animationStepThree = false;
+            } else if (this._startAnimation) {
+                console.log("_startAnimation");
+                this._startAnimation = false;
+
+                point = this.point;
+                elementX = point.x;
+                elementY = point.y;
+
+                screenWidth = this.screenWidth;
+                screenHeight = this.screenHeight;
+
+                if (imageWidth * screenHeight > imageHeight * screenWidth) {
+                    targetWidth = imageWidth * screenHeight / imageHeight;
+                    targetHeight = screenHeight;
+                } else {
+                    targetWidth = screenWidth;
+                    targetHeight = imageHeight * screenWidth / imageWidth;
+                }
+                targetWidth |= 0;
+                targetHeight |= 0;
+                if (imageWidth * elementHeight > imageHeight * elementWidth) {
+                    uncroppedWidth = imageWidth * elementHeight / imageHeight;
+                    //                 uncroppedHeight = elementHeight;
+                    uncroppedX = elementX - (uncroppedWidth - elementWidth) * 0.5;
+                    uncroppedY = elementY;
+                    zoom = elementHeight / targetHeight;
+                    clip = (targetWidth - (elementWidth * targetHeight / elementHeight)) * 0.5;
+                    zoomedImage.style.webkitTransform = "translate3d(" + clip + "px,0,0)";
+                    clip2.style.webkitTransform = "translate3d(" + (-clip * 2) + "px,0,0)";
+                    clip1.style.webkitTransform = "translate3d(" + clip + "px,0,0)";
+                } else {
+                    //                 uncroppedWidth = elementWidth;
+                    uncroppedHeight = imageHeight * elementWidth / imageWidth;
+                    uncroppedX = elementX;
+                    uncroppedY = elementY - (uncroppedHeight - elementHeight) * 0.5;
+                    zoom = elementWidth / targetWidth;
+                    clip = (targetHeight - (elementHeight * targetWidth / elementWidth)) * 0.5;
+                    zoomedImage.style.webkitTransform = "translate3d(0," + clip + "px,0)";
+                    clip2.style.webkitTransform = "translate3d(0," + (-clip * 2) + "px,0)";
+                    clip1.style.webkitTransform = "translate3d(0," + clip + "px,0)";
+                }
+                clippedZoomedImage.style.width = targetWidth + "px";
+                clippedZoomedImage.style.height = targetHeight + "px";
+                zoomedImage.style.width = targetWidth + "px";
+                zoomedImage.style.height = targetHeight + "px";
+                clip1.style.width = targetWidth + "px";
+                clip1.style.height = targetHeight + "px";
+                clip2.style.width = targetWidth + "px";
+                clip2.style.height = targetHeight + "px";
+                spacer.style.width = targetWidth + "px";
+                spacer.style.height = targetHeight + "px";
+                if (imageWidth * screenHeight > imageHeight * screenWidth) {
+                    uncroppedX += (targetWidth - screenWidth) >> 1;
+                    zoomedImageWrapper.classList.remove("scroll");
+                    zoomedImageWrapper.scrollLeft = (targetWidth - screenWidth) >> 1;
+                    zoomedImageWrapper.scrollTop = 0;
+                } else {
+                    zoomedImageWrapper.classList.remove("scroll");
+                    zoomedImageWrapper.scrollLeft = 0;
+                    uncroppedY += (targetHeight - screenHeight) >> 1;
+                    zoomedImageWrapper.scrollTop = (targetHeight - screenHeight) >> 1;
+                }
+                clippedZoomedImage.style.webkitTransform = "translate3d(" + uncroppedX + "px," + uncroppedY + "px,0) scale3d(" + zoom + "," + zoom + ",1)";
+                clippedZoomedImage.style.webkitTransition = "none";
+                zoomedImage.style.webkitTransition = "none";
+                clip2.style.webkitTransition = "none";
+                clip1.style.webkitTransition = "none";
+                clippedZoomedImage.style.opacity = 0;
+
+                this._animationStepTwo = true;
+                this.needsDraw = true;
+            } else if (this._animationStepTwo) {
+                console.log("_animationStepTwo");
+                this._animationStepTwo = false;
+
+                zoomedImageWrapper.classList.add("scroll");
+                clippedZoomedImage.style.webkitTransition = "500ms -webkit-transform ease-in-out";
+                zoomedImage.style.webkitTransition = "500ms -webkit-transform ease-in-out";
+                clip2.style.webkitTransition = "500ms -webkit-transform ease-in-out";
+                clip1.style.webkitTransition = "500ms -webkit-transform ease-in-out";
+
+                this._animationStepThree = true;
+                this.needsDraw = true;
+            } else if (this._animationStepThree) {
+                console.log("_animationStepThree");
+                this._animationStepThree = false;
+
+                clippedZoomedImage.style.webkitTransform = "translate3d(0,0,0) scale3d(1,1,1)";
+                zoomedImage.style.webkitTransform = "translate3d(0,0,0)";
+                clip2.style.webkitTransform = "translate3d(0,0,0)";
+                clip1.style.webkitTransform = "translate3d(0,0,0)";
+                clippedZoomedImage.style.opacity = 1;
             }
         }
     },
@@ -96,10 +193,8 @@ exports.Detail = Component.specialize(/** @lends Detail# */ {
     didShowOverlay: {
         value: function () {
             var self = this;
-            setTimeout(function () {
-                self._startAnimation = true;
-                self.needsDraw = true;
-            }, 100);
+            self._startAnimation = true;
+            self.needsDraw = true;
             setTimeout(function () {
                 self._animationFinished = true;
                 if(self._imageSource) {
